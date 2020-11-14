d4df50b14a51984ad8245fdbdef1ec2e4d577a86
Subramanyam.D 24M <57181459+Subbu23M@users.noreply.github.com>
2020-11-14 18:21:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// When the window loads the entire images, the pre-loader should hide.
+
+window.addEventListener('load',function(){
+    document.querySelector('.pre-loader').style.display = 'none';
+});
